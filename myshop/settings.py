7916710a76f31ee820b86e7a786f533580cb4a1b
Anton Shefer <rezajkee@mail.ru>
2022-12-12 23:54:28 +0500

    Add functionality for basic product catalog

--- myshop/settings.py
+++ myshop/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Media settings
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
