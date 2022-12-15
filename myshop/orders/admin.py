1b4333f45880ae0cc528a949291edfdfd1129cfd
Anton Shefer <rezajkee@mail.ru>
2022-12-15 21:25:55 +0500

    Add exporting orders to CSV files

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
+import csv
+import datetime
+from django.http import HttpResponse
 
 from .models import Order, OrderItem
 
@@ -12,7 +15,33 @@ def order_payment(obj):
     return ""
 
 
+# Description for the admin panel
 order_payment.short_description = "Stripe payment"
+
+
+def export_to_csv(modeladmin, request,queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f"attachment; filename={opts.verbose_name}.csv"
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write a first row with header information
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime("%d/%m/%Y")
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+# Description for the admin panel
+export_to_csv.short_description = "Export to CSV"
 
 
 class OrderItemInline(admin.TabularInline):
@@ -37,3 +66,4 @@ class OrderAdmin(admin.ModelAdmin):
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
